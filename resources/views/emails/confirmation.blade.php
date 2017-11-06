d3fbff31e17aaf836c00e398606c0a0dcd6ad7c9
Leisia Medeiros <leisiamedeiros@gmail.com>
2017-11-05 23:49:30 -0300

    change mail view message

--- resources/views/emails/confirmation.blade.php
+++ resources/views/emails/confirmation.blade.php
@@ -1,21 +1,18 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-    </head>
-    <body>
-        <div class="content">
-          <h1>Obrigado por se registrar!</h1>
-
-          <p>
-            Voce precisa confirmar seu email, por favor clique
-            <a href="{{ url('/register/confirm/'.$user->token) }}">aqui</a> para confirmar.
-          </p>
-        </div>
-    </body>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>{{ config('app.name') }}</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1>Obrigado por se registrar!</h1>
+      <p>
+        Voce precisa confirmar seu email, por favor clique
+        <a href="{{ url('/register/confirm/'.$user->token) }}">aqui</a> para confirmar.
+      </p>
+      Obrigada,<br>
+      {{ config('app.name') }}
+    </div>
+  </body>
 </html>
